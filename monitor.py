bb30f11fe79d255ff38e4219f1658f1c5a7f6fe0
s812v <shi8124verma@gmail.com>
2025-07-24 18:54:40 +0530

    removed idle check

--- monitor.py
+++ monitor.py
@@ -6,7 +6,6 @@ import logging
 import threading
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from py_idle_time import get_idle_duration
 
 MONITORED_DIRECTORY = r'C:\Users\Shireen\OneDrive\Desktop\VSCode Codes' #r stands for raw string
 INACTIVITY_LIMIT = 30 * 60    # 30 minutes of no file modification/creation
@@ -34,9 +33,9 @@ class fileHandler(FileSystemEventHandler):
     def inactivity_monitor(self):
         while(True):
             time.sleep(10)  # every 10 seconds
-            idle_time = get_idle_duration() # mouse/keyboard movement
+           # idle_time = get_idle_duration() # mouse/keyboard movement
             inactive_time = time.time() - self.lastActive # last file modification within directory
-            if self.sessionActive and (inactive_time > INACTIVITY_LIMIT or idle_time > INACTIVITY_LIMIT):
+            if self.sessionActive and (inactive_time > INACTIVITY_LIMIT):
                 sessionEnd = datetime.datetime.now()
                 duration = sessionEnd - self.sessionStart
 
