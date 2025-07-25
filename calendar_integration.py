fb9e6537d621ecd99bb477a0323428eba5017dfc
s812v <shi8124verma@gmail.com>
2025-07-25 16:58:27 +0530

    update calendar connection

---
+++ calendar_integration.py
@@ -0,0 +1,65 @@
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+CALENDAR_ID = "3a1773ac22fdcc8632d114dd38affc3b05dfe4ae3018628e6357c952c95e3744@group.calendar.google.com"
+TIMEZONE = "Asia/Kolkata"
+
+def getCreds():
+  """Shows basic usage of the Google Calendar API.
+  Prints the start and name of the next 10 events on the user's calendar.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+
+def addEvent(start_time, end_time, description):
+  creds = getCreds() 
+  service = build("calendar", "v3", credentials=creds)
+
+  event = {
+    'summary': 'Coding Session',
+    'description': description,
+    'start': {
+      'dateTime': start_time.isoformat(),
+      'timeZone': TIMEZONE,
+    },
+    'end': {
+      'dateTime': end_time.isoformat(),
+      'timeZone': TIMEZONE,
+    },
+  }
+  created_event = service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
+  print(f"Event created: {created_event.get('htmlLink')}")
+
+#testing
+if __name__ == "__main__":
+  creds = getCreds()
+  if creds == None:
+    print("Google Calendar setup failed")
+  else:
+    print("Calendar setup is working")
--- monitor.py
+++ monitor.py
@@ -1,5 +1,8 @@
 # file modification watching & idle check
 
+from calendar_integration import addEvent
+
+import os
 import time
 import datetime
 import logging
@@ -17,11 +20,12 @@ class fileHandler(FileSystemEventHandler):
         self.sessionActive = False  #session start/stop
         self.sessionStart = None    #start time
         self.lastActive = time.time() #last activity time
+        self.project = None    #last modified folder ie project worked on during session
         self.inactivity_thread = threading.Thread(target=self.inactivity_monitor)  #idle checking
-        self.daemon = True # allows the main thread to exit even if this thread is running
+        self.inactivity_thread.daemon = True # allows the main thread to exit even if this thread is running
         self.inactivity_thread.start()
-
-    def on_modified(self,event):
+        
+    def on_modified(self, event):
         #print(f'File {event.src_path} was modified')
         self.lastActive = time.time()
         
@@ -30,6 +34,32 @@ class fileHandler(FileSystemEventHandler):
             print(f"Session started at {self.sessionStart}")
             self.sessionActive = True
     
+    def get_last_modified_folder(self):
+        # 'project folder' will be added as part of the event name
+        
+        if not os.path.isdir(MONITORED_DIRECTORY):
+            print(f"Error: Directory '{MONITORED_DIRECTORY}' does not exist.")
+            self.project = "Error"
+            return
+       
+        subdirectories = [d for d in os.listdir(MONITORED_DIRECTORY) if os.path.isdir(os.path.join(MONITORED_DIRECTORY, d))]
+        
+        if not subdirectories:
+            self.project = "General"
+            return   # no subdirectories found
+
+        latest_mod_time = 0
+        last_modified_folder = None
+
+        for subdir in subdirectories:
+            full_path = os.path.join(MONITORED_DIRECTORY, subdir)
+            mod_time = os.path.getmtime(full_path)
+            if mod_time > latest_mod_time:
+                latest_mod_time = mod_time
+                last_modified_folder = subdir
+        
+        self.project = last_modified_folder
+
     def inactivity_monitor(self):
         while(True):
             time.sleep(10)  # every 10 seconds
@@ -38,13 +68,12 @@ class fileHandler(FileSystemEventHandler):
             if self.sessionActive and (inactive_time > INACTIVITY_LIMIT):
                 sessionEnd = datetime.datetime.now()
                 duration = sessionEnd - self.sessionStart
-
                 if duration.total_seconds() > INVALIDITY_DURATION: # long session
-                    print(f"Session ended at {sessionEnd}, Duration: {duration}")
-                    #addEvent here
+                    self.get_last_modified_folder()
+                    print(f"Session ended at {sessionEnd}, Duration: {duration}, Project: {self.project}")
+                    addEvent(self.sessionStart,sessionEnd, str(self.project))
                 self.sessionActive = False
                     
-
 # runs script until keyboard interrupt
 if __name__ == "__main__":
     # initialize logging event handler
